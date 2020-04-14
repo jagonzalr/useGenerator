fafd3852c3a11ef9af0a7039cb277cd235f0c2c3
Jose Antonio Gonzalez <jagonzalr@gmail.com>
2020-04-14 14:43:08 +0200

    Update demo and documentation

--- README.md
+++ README.md
@@ -1,9 +1,54 @@
+[![npm version](https://badge.fury.io/js/use-generator.svg)](https://badge.fury.io/js/use-generator)
+
 # useGenerator
 
-Generator using React hooks.
+[Generator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator) using [React hooks](https://reactjs.org/docs/hooks-intro.html).
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Demo](#demo)
+- [References](#references)
+- [Images](#images)
+- [License](#license)
+
+## Getting started
+
+`npm install --save use-generator`
+
+## Usage
+
+```
+import { useGenerator } from 'use-generator'
+const [ipAddress, setIpAddress] = useState(null)
+useGenerator(function* () {
+	const ipAddressResponse = yield fetch('https://api.ipify.org').then(data => data.text())
+	setIpAddress(ipAddressResponse.value)
+}, [])
+```
+
+## Demo
+
+```
+git clone git@github.com:jagonzalr/useMediaRecorder.git
+cd useGenerator
+npm intall
+npm start
+```
+
+## References
+
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Generator
+- https://dev.to/n1ru4l/homebrew-react-hooks-useasynceffect-or-how-to-handle-async-operations-with-useeffect-1fa8
+- https://gist.github.com/ericelliott/890c20d18bcc4362048dba2dca8e67ac
 
 ## Images
 
 - https://www.pexels.com/photo/brown-and-gray-rock-formation-1884306/
 - https://www.pexels.com/photo/white-rock-formation-and-blue-body-of-water-2259495/
 - https://www.pexels.com/photo/brown-rock-near-body-of-water-2422785/
+
+## License
+
+useGenerator is [MIT licensed](./LICENSE).
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-// https://dev.to/n1ru4l/homebrew-react-hooks-useasynceffect-or-how-to-handle-async-operations-with-useeffect-1fa8
-// https://gist.github.com/ericelliott/890c20d18bcc4362048dba2dca8e67ac
 'use strict'
 
 import { useEffect, useRef } from 'react'
-
-function* cast(input) {
-  return yield input
-}
 
 export const useGenerator = generator => {
   const generatorRef = useRef(generator)
@@ -14,22 +8,22 @@ export const useGenerator = generator => {
   useEffect(() => {
     let ignore = false
     if (generatorRef.current) {
-      const instance = generatorRef.current(cast)
+      const genFunc = generatorRef.current()
 
-      const run = async instance => {
-        let result = { value: undefined, done: false }
+      const execute = async () => {
+        let result = { value: null, done: false }
         while (!result.done) {
           try {
             if (ignore) return
-            result = instance.next(result.value)
+            result = genFunc.next(result.value)
 
             try {
-              const response = await result.value
-              result.value = { response, error: null }
+              const value = await result.value
+              result.value = { value, error: null }
             } catch (err) {
               if (ignore) return
               const error = await err
-              result.value = { response: null, error: error.message }
+              result.value = { value: null, error: error.message }
             }
           } catch (err) {
             console.error(`useGenerator - unhandled error: ${err.message}`)
@@ -38,11 +32,10 @@ export const useGenerator = generator => {
         }
       }
 
-      run(instance)
+      execute()
     }
 
     return () => {
-      console.log('here')
       ignore = true
     }
   }, [generatorRef.current])
